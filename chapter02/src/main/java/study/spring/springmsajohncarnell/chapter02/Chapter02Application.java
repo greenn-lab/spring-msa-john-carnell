00cfa9c77ceeba8c6fe10b7a5eca77969bc7354c
greenn-lab <greenn.lab@gmail.com>
2020-07-06 23:12:15 +0900

    ch03 completed

--- chapter02/src/main/java/study/spring/springmsajohncarnell/chapter02/Chapter02Application.java
+++ chapter02/src/main/java/study/spring/springmsajohncarnell/chapter02/Chapter02Application.java
@@ -2,8 +2,10 @@ package study.spring.springmsajohncarnell.chapter02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class Chapter02Application {
   
   public static void main(String[] args) {
