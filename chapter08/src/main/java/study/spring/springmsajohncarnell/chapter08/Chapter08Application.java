715420e8d17e5bf4e763d0def49a6d3cced0f0cf
greenn-lab <greenn.lab@gmail.com>
2020-08-17 22:55:00 +0900

    ch08 completed

---
+++ chapter08/src/main/java/study/spring/springmsajohncarnell/chapter08/Chapter08Application.java
@@ -0,0 +1,13 @@
+package study.spring.springmsajohncarnell.chapter08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter08Application {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(Chapter08Application.class, args);
+  }
+  
+}
---
+++ chapter08/src/test/java/study/spring/springmsajohncarnell/chapter08/Chapter08ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.springmsajohncarnell.chapter08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
